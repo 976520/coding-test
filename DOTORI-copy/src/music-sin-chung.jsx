9a46b27d75d46c45714dee031ef53217cb387b9a
haensol <haensol976520@gmail.com>
2024-04-16 21:14:58 +0900

    COPYCAT

---
+++ DOTORI-copy/src/music-sin-chung.jsx
@@ -0,0 +1,50 @@
+const root = document.getElementById('music-sin-chung-root');
+function App() {
+    const [music, setMusic] = React.useState();
+    const [number, setNumber] = React.useState(0);
+    const [text, setText] = React.useState();
+    const [thumbnail, setThumbnail] = React.useState();
+    const onChange = (event) => {
+        console.log(event.target.value);
+        setMusic(event.target.value);
+        const inputText = event.target.value;
+        setText(inputText);
+    }
+
+    function click() {
+        console.log(music);
+        setNumber(number + 1);
+        const container = document.getElementById("sin-chung-container")
+        const tomato = document.createElement("div");
+        tomato.id = 'music';
+        tomato.textContent = music;
+        container.appendChild(tomato);
+        setText('');
+    }
+    
+    return(
+        <div>
+            <div id="music-sin-chung">
+                <h2>음악 신청</h2>
+                <input 
+                    type="url" //?
+                    value={text}
+                    id="url-input"
+                    onChange={onChange}
+                ></input>
+                <div id="sin-chug-ha-gi" onClick={click}>신청하기</div>
+                
+            </div>
+
+            <div id="sin-chung-list">
+                <h2>신청음악</h2>
+                <p><span>{number}</span> 개</p>
+                <div id="sin-chung-container">
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+ReactDOM.render(<App />, root);
