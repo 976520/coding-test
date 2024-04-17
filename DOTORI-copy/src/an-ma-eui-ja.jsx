437eb561fd3c55b06ca83462871b789e811efdea
haensol <haensol976520@gmail.com>
2024-04-18 08:02:25 +0900

    COPYCAT

--- DOTORI-copy/src/an-ma-eui-ja.jsx
+++ DOTORI-copy/src/an-ma-eui-ja.jsx
@@ -2,25 +2,25 @@ const root = document.getElementById("root-an-ma");
 let anMaSinChung = "안마의자";
 let background = '#6568E0'
 let color = 'white';
+let newCounter = 0;
+let localCounter = parseInt(localStorage.getItem("anma-counter")) || 0;
     function App() {
 
-        const [counter, modifier] = React.useState(0);
+        const [counter, modifier] = React.useState(localCounter);
 
-        let localCounter = localStorage.getItem("anma-counter") || 0;
-
-        let anMaGauge = (localCounter/5)*500;
+        let anMaGauge = (counter /5)*500;
         let anMaRadius = "5px 0px 0px 5px";
         let backgroundColor = "#93CB7A";
 
-        if (localCounter < 3) {
+        if (counter < 3) {
             backgroundColor = "#93CB7A";
-        } else if (3 <= localCounter && localCounter < 4) {
+        } else if (3 <= counter && counter < 4) {
             backgroundColor = "#E2D861";
-        } else if (4 <= localCounter && localCounter <= 5) {
+        } else if (4 <= counter && counter <= 5) {
             backgroundColor = "#D67E72";
         }
 
-        if (localCounter == 5) {
+        if (counter == 5) {
             anMaSinChung = "신청불가";
             background = "#282A59"
             color="#7A7990"
@@ -28,18 +28,18 @@ let color = 'white';
         }
         
         const onClick = () => {
-            localCounter = parseInt(localStorage.getItem("anma-counter"))
-            if(localCounter != 5) {
+            
+            if(counter != 5) {
                 const newCounter = counter + 1;
                 modifier(newCounter);
-                localStorage.setItem("anma-counter", newCounter) || 0;
+                localStorage.setItem("anma-counter", newCounter.toString()) || 0;
             }
         };
 
         return(
             <div>
                 <div id="an-ma-people-container">
-                    <div className="sin-chung-people">{localCounter}/5</div>
+                    <div className="sin-chung-people">{counter}/5</div>
                     <div id="an-ma-gauge-background">
                         <div id="an-ma-gauge" style={{width: anMaGauge, borderRadius: anMaRadius, backgroundColor: backgroundColor}}></div>
                     </div>
--- DOTORI-copy/src/ja-sup-sin-chung.jsx
+++ DOTORI-copy/src/ja-sup-sin-chung.jsx
@@ -1,57 +1,51 @@
-
 const root = document.getElementById("root-ja-sup");
 let jaSupSinChung = "자습신청";
-let jaSupBorder = '#6568E0'
+let jaSupBorder = '#6568E0';
 let jaSupColor = 'white';
+let newCounter = 0;
+let localCounter = parseInt(localStorage.getItem("jasup-counter")) || 0;
+
 function App() {
-        
-        const [counter, modifier] = React.useState(0);
-        
-        let localCounter = localStorage.getItem("jasup-counter") || 0;
+    const [counter, setCounter] = React.useState(localCounter);
 
-        let jaSupGauge = (localCounter/50)*500;
-        let jaSupRadius = "5px 0px 0px 5px";
-        let backgroundColor = "#93CB7A";
+    let jaSupGauge = (counter / 50) * 500;
+    let jaSupRadius = "5px 0px 0px 5px";
+    let backgroundColor = "#93CB7A";
 
-        if (localCounter < 30) {
-            backgroundColor = "#93CB7A";
-        } else if (30 <= localCounter && localCounter < 40) {
-            backgroundColor = "#E2D861";
-        } else if (40 <= localCounter && localCounter <= 50) {
-            backgroundColor = "#D67E72";
+    if (counter < 30) {
+        backgroundColor = "#93CB7A";
+    } else if (30 <= counter && counter < 40) {
+        backgroundColor = "#E2D861";
+    } else if (40 <= counter && counter <= 50) {
+        backgroundColor = "#D67E72";
+    }
+
+    if (counter === 50) {
+        jaSupSinChung = "신청불가";
+        jaSupBorder = "#282A59";
+        jaSupColor = "#7A7990";
+        jaSupRadius = "5px 5px 5px 5px";
+    }
+
+    const onClick = () => {
+        if (counter < 50) {
+            const newCounter = counter + 1;
+            setCounter(newCounter);
+            localStorage.setItem("jasup-counter", newCounter.toString());
         }
+    };
 
-        if (localCounter == 50) {
-            jaSupSinChung = "신청불가";
-            jaSupBorder = "#282A59"
-            jaSupColor="#7A7990"
-            jaSupRadius = "5px 5px 5px 5px";
-        }
-
-        const onClick = () => {
-            localCounter = parseInt(localStorage.getItem("jasup-counter"))
-            jaSupGauge = (localCounter/5)*500;
-            if(localCounter != 50) {
-                const newCounter = counter + 1;
-                modifier(newCounter);
-                localStorage.setItem("jasup-counter", newCounter.toString());
-            }
-            
-        };
-
-        return(
-            <div>
-                <div id="ja-sup-people-container">
-                    <div className="sin-chung-people">{localCounter}/50</div>
-                    <div id="ja-sup-gauge-background">
-                        <div id="ja-sup-gauge" style={{width: jaSupGauge, borderRadius: jaSupRadius, backgroundColor: backgroundColor}}></div>
-                    </div>
+    return (
+        <div>
+            <div id="ja-sup-people-container">
+                <div className="sin-chung-people">{counter}/50</div>
+                <div id="ja-sup-gauge-background">
+                    <div id="ja-sup-gauge" style={{ width: jaSupGauge, borderRadius: jaSupRadius, backgroundColor: backgroundColor }}></div>
                 </div>
-                <div id="ja-sup-button" style={{background: jaSupBorder, color: jaSupColor}} onClick={onClick}>{jaSupSinChung}</div>
-                
             </div>
-        );
-        
+            <div id="ja-sup-button" style={{ background: jaSupBorder, color: jaSupColor }} onClick={onClick}>{jaSupSinChung}</div>
+        </div>
+    );
 }
 
-ReactDOM.render(<App />, root); 
+ReactDOM.render(<App />, root);
