47f4b08305d5d2b3a30a20f9e77edd1dccb29cff
haensol <haensol976520@gmail.com>
2024-04-17 09:42:16 +0900

    COPYCAT

--- DOTORI-copy/src/music-sin-chung.jsx
+++ DOTORI-copy/src/music-sin-chung.jsx
@@ -1,27 +1,59 @@
 const root = document.getElementById('music-sin-chung-root');
 function App() {
-    const [music, setMusic] = React.useState();
+    const [musicUrl, setMusicUrl] = React.useState();
     const [number, setNumber] = React.useState(0);
     const [text, setText] = React.useState();
+    const [n, setn] = React.useState(0);
+    const [videoId, setVideoId] = React.useState();
     const [thumbnail, setThumbnail] = React.useState();
     const onChange = (event) => {
-        console.log(event.target.value);
-        setMusic(event.target.value);
+        setMusicUrl(event.target.value);
+        //setThumbnail(~~);
         const inputText = event.target.value;
         setText(inputText);
     }
 
     function click() {
-        console.log(music);
+        if(text == ''){
+            return 0;
+        }
         setNumber(number + 1);
-        const container = document.getElementById("sin-chung-container")
+        setVideoId(text.match(/v=\/(\w+)\//));
+        
+        setn(n+1);
+        console.log(n);
+        const container = document.getElementById("sin-chung-container");
+
         const tomato = document.createElement("div");
-        tomato.id = 'music';
-        tomato.textContent = music;
+        tomato.className = 'music';
+        tomato.id = 'music'+ n;
+        tomato.textContent = musicUrl;
         container.appendChild(tomato);
         setText('');
+
+        const potato = document.createElement("img");
+        potato.className = "thumbnail";
+        potato.id="thumbnail"+n;
+        potato.src = 'https://img.youtube.com/vi/'+videoId+'/mqdefault.jpg'; 
+        tomato.appendChild(potato);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = 'delete-button';
+        deleteButton.id= 'delete-button' + n;
+        deleteButton.addEventListener("click", () => deleteElement(n));
+        tomato.appendChild(deleteButton);
+        
+        function deleteElement(n) {
+            console.log(n);
+            const element = document.getElementById('music'+ n);
+            tomato.remove(element);
+            setNumber(number => number-1); 
+            
+        }
     }
     
+    
+
     return(
         <div>
             <div id="music-sin-chung">
